0db2e133d575ff7a53f0d47dd1ee4d8afb39c307
sanjh chhatrasal <sanjhchhatrasal@gmail.com>
2025-02-22 18:00:45 +0530

    menu and  app-page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,6 @@
-import Link from "next/link";
+"use client"
+
+
 import styles from "./homepage.module.css";
 import Featured from "@/components/Featured/Featured";
 import CategoryList from "@/components/CategoryList/CategoryList";
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import styles from "./Menu.module.css";
 import MenuPosts from "../menuPosts/MenuPosts";
